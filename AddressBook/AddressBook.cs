43a2b95de97d5129aa2b11561e2c8da60b619d85
“Pratik” <pratikbhandurge2222@gmail.com>
2022-09-12 14:09:11 +0530

    AddedUc11Solution

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -4,32 +4,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBookProgram
+namespace AddressBook
 {
-    internal class AddressBook
+    class AddressBook
     {
-        public String firstName;
-        public String lastName;
-        public String address;
-        public String city;
-        public String state;
-        public long zip;
-        public long phoneNumber;
-        public String email;
+        public List<Contact> contactList;
 
-        static public void SetAddress()
+
+        public AddressBook()
         {
+            contactList = new List<Contact>();
 
-            AddressBook addressBook = new AddressBook();
-            addressBook.firstName = "pratik";
-            addressBook.lastName = "Bha";
-            addressBook.address = "W Pune";
-            addressBook.city = "Pune";
-            addressBook.state = "Maharashtra";
-            addressBook.zip = 12354;
-            addressBook.phoneNumber = 6789009876;
-            addressBook.email = "pratik@gmail.com";
-            Console.WriteLine(addressBook.firstName);
+        }
+        public string AddContact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNo, string emailId)
+        {
+            if (CheckName(firstName, lastName) == false)
+            {
+                Contact contact = new Contact(firstName, lastName, address, city, state, zipCode, phoneNo, emailId);
+                contactList.Add(contact);
+                return "Details Added Successfully";
+            }
+            return "Name already exists";
+        }
+        public void EditContact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNo, string emailId)
+        {
+            foreach (Contact c in contactList)
+            {
+                if (c.firstName.Equals(firstName))
+                {
+                    c.lastName = lastName;
+                    c.address = address;
+                    c.city = city;
+                    c.state = state;
+                    c.zipCode = zipCode;
+                    c.phoneNo = phoneNo;
+                    c.emailId = emailId;
+
+                    return;
+                }
+            }
+        }
+        public void RemoveContact(string firstName, string lastName)
+        {
+            foreach (Contact c in contactList)
+            {
+                if (c.firstName.Equals(firstName) && c.lastName.Equals(lastName))
+                {
+                    contactList.Remove(c);
+
+                    return;
+                }
+            }
+        }
+        public bool CheckName(string firstName, string lastName)
+        {
+            foreach (Contact c in contactList.FindAll(e => e.firstName.Equals(firstName) && e.lastName.Equals(lastName)))
+            {
+                return true;
+            }
+            return false;
+        }
+        public List<Contact> GetPersonByCityOrState(string cityOrState)
+        {
+            List<Contact> contact = new List<Contact>();
+            foreach (Contact c in contactList.FindAll(e => e.city.Equals(cityOrState) || e.state.Equals(cityOrState)))
+            {
+                contact.Add(c);
+            }
+            return contact;
+        }
+        public void SortByName()
+        {
+            contactList.Sort((contact1, contact2) => contact1.firstName.CompareTo(contact2.firstName));
+            foreach (Contact c in contactList)
+            {
+                Console.WriteLine(c.ToString());
+            }
         }
     }
 }
